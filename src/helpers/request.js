2759e34fad6b5e65b646640a51e45be6aa27d5c9
liuhuiliu <765226421@qq.com>
2021-03-18 11:59:18 +0800

    3.18

---
+++ src/helpers/request.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import { Message } from 'element-ui'
+
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.baseURL = '//localhost:8080';
+// axios.defaults.baseURL = 'https://api.example.com';
+window.request = request
+
+export default function request(url, type = 'GET', data = {}) {
+    return new Promise((resolve, reject) => {
+            let option = {
+                url,
+                method: type,
+            }
+            if (type.toLocaleLowerCase() === 'get') {
+                option.params = data
+            }
+            if (localStorage.token) {
+                axios.defaults.headers.common['Authorization'] = localStorage.token
+            }
+
+            axios(option).then(res => {
+                console.log(res.data)
+                if (res.data.status === 'ok') {
+                    if (res.data.token) {
+                        localStorage.token = res.data.token
+                    }
+                    resolve(res.data)
+                } else {
+                    Message.error(res.data.msg)
+                    reject(res.data)
+                }
+
+            }).catch(erro => {
+                Message.error('网络异常')
+                reject({ msg: '网络异常' })
+            })
+        }
+
+    )
+}
--- src/pages/Index/Index.vue
+++ src/pages/Index/Index.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="index">
    index页面
+   <p>
+     test
+   </p>
+   <el-button @click="onClick1">默认按钮1</el-button>
+   <el-button @click="onClick2">默认按钮2</el-button>
   </div>
+  
 </template>
 
 <script>
@@ -11,14 +17,33 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  methods:{
+    onClick1(){
+      this.$message('hello')
+    },
+    onClick2(){
+       this.$alert('test content', 'title', {
+          confirmButtonText: 'button text',
+          callback: action => {
+            console.log('done')
+            this.$message('hello')
+          }
+        });
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-#index
-{
-  color: red;
-}
+
+<style lang="less" scoped >
+@import "../../assets/base.less";
+//  #index{
+//     color:red;
+//   }
+  p{
+    color: @themeColor;
+  }
+ 
 </style>
