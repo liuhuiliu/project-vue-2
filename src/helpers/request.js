2c560a5b3da78e164015ae00e1c50d85f380b6d8
liuhuiliu <765226421@qq.com>
2021-07-27 21:08:15 +0800

    final

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -3,8 +3,8 @@ import { Message } from 'element-ui'
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 // axios.defaults.baseURL = '//localhost:8080';
-axios.defaults.baseURL = 'https://blog-server.hunger-valley.com/';
-
+axios.defaults.baseURL = '//blog-server.hunger-valley.com/';
+// axios.defaults.baseURL = 'https://blog-server.hunger-valley.com/';
 window.request = request
 
 export default function request(url, type = 'GET', data = {}) {
